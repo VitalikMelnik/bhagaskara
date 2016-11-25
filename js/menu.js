db07c22e380c634e76b1ed24503a75417c0ed99b
VitalikMelnik <vitalikmelnik20000@gmail.com>
2016-11-25 21:54:42 +0200

    first commit

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+/**
+ * Created by vitalik on 25.11.16.
+ */
+$(document).ready(function () {
+
+    var menu = $('.menu');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.menu').addClass('sticky');
+            
+        } else {
+            $('.menu').removeClass('sticky');
+            
+        }
+
+
+    }
+
+    document.onscroll = scroll;
+
+});
---
+++ js/root.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vitalik on 25.11.16.
+ */
+
+
+angular.module('app',[]).controller('rootController',function ($scope) {
+
+    
+})
+
